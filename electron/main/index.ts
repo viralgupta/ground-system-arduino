c5a97fef866b86d9765e2474f4b92de0dbeeb129
viralgupta <guptaviral0@gmail.com>
2024-05-31 21:46:23 +0530

    chore

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -7,6 +7,8 @@ import os from "node:os";
 const require = createRequire(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+if (require('electron-squirrel-startup')) app.quit();
+
 process.env.APP_ROOT = path.join(__dirname, "../..");
 
 export const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
